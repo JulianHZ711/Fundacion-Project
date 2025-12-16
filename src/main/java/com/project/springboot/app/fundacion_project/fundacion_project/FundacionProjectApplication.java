2be1d2f00ebbac2349e1a2fa727a737e685a2062
J. cob <jacoboosorio299589@gmail.com>
2025-12-16 11:41:14 -0500

    Creating the spring boot project with maven

---
+++ src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/FundacionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot.app.fundacion_project.fundacion_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundacionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundacionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springboot/app/fundacion_project/fundacion_project/FundacionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.app.fundacion_project.fundacion_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundacionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
