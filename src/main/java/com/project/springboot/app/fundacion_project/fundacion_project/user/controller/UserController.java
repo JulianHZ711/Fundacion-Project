59d44b3bfe5b33b43122869e01bc212ecbcf9f5f
J. cob <jacoboosorio299589@gmail.com>
2026-01-02 18:49:10 -0500

    Fixing warnings in UserController file

--- src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/user/controller/UserController.java
+++ src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/user/controller/UserController.java
@@ -18,7 +18,6 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
