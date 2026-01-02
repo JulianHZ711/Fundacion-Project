a13036d3a91cffc393c713ce9315d3d99ba04876
J. cob <jacoboosorio299589@gmail.com>
2026-01-02 18:32:48 -0500

    Adding the user role to the user, verify if its an admin and also encoding the user's password

--- src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/user/service/UserServiceImpl.java
+++ src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/user/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.project.springboot.app.fundacion_project.fundacion_project.user.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.springboot.app.fundacion_project.fundacion_project.role.Role;
 import com.project.springboot.app.fundacion_project.fundacion_project.role.repository.RoleRepository;
 import com.project.springboot.app.fundacion_project.fundacion_project.user.User;
 import com.project.springboot.app.fundacion_project.fundacion_project.user.repository.UserRepository;
@@ -32,6 +35,22 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public User save(User user) {
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(role -> roles.add(role));
+
+        if(user.isAdmin()){
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(role -> roles.add(role));
+        }
+
+        user.setRoles(roles);
+
+        //Encoding the password using PasswordEncoder
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         return repository.save(user);
     }
 
