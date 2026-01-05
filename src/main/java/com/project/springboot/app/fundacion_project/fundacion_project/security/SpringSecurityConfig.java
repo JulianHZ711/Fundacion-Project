fadb6d2498f2196d5e1d753fe0555adeaf570bb5
J. cob <jacoboosorio299589@gmail.com>
2026-01-05 17:29:11 -0500

    Adding the JWT filters for the requests in SpringSecurityConfig

--- src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/security/SpringSecurityConfig.java
+++ src/main/java/com/project/springboot/app/fundacion_project/fundacion_project/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.project.springboot.app.fundacion_project.fundacion_project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -12,5 +15,18 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //Adding the filters to validate all the requests, authorize or deny permissions, etc.
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //All the user routes are public in order for users to create their own account, but the rest of the
+        // routes are denied because the user needs to be authenticated.
+        return http.authorizeHttpRequests( (auth) -> auth
+        .requestMatchers("/users").permitAll()
+        .anyRequest().authenticated())
+        .csrf(config -> config.disable()) // To avoid vulnerabilities
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build(); //Session creationg policy will be STATELESS so everything authentication-related will be handled by the token
     }
 }
