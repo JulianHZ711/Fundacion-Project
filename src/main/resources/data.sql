2a47b846f8ff268f66d25ffcad5da6ddc18f96ed
J. cob <jacoboosorio299589@gmail.com>
2026-01-16 18:39:35 -0500

    Deleting the user insert inside data.sql since is no longer useful. Token validation pending for requests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,3 @@ INSERT INTO ROLES (name) VALUES ("ROLE_COORDINATOR");
 INSERT INTO ROLES (name) VALUES ("ROLE_NUTRITIONIST");
 INSERT INTO ROLES (name) VALUES ("ROLE_PSYCHOLOGIST");
 INSERT INTO ROLES (name) VALUES ("ROLE_SOCIAL_WORKER");
-
---Creating an admin user in order to create and test new users
-INSERT INTO USERS (role_id, password, username, enabled) VALUES (1, "AdminPassword", "admin_test", TRUE)
